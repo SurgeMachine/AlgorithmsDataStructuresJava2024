24ad53d33b1598959e7041fa849ffe67cc3ac082
Surge <serhii.matchyn@stud.onu.edu.ua>
2024-10-22 00:50:49 +0300

    ArrayLists

--- HashMaps/src/GradeBook.java
+++ HashMaps/src/GradeBook.java
@@ -7,30 +7,30 @@ public class GradeBook {
     /*Створіть програму для управління оцінками студентів. Кожен студент має
     унікальний ідентифікаційний номер (ID), ім’я та список оцінок.
     Використовуйте HashMap для зберігання інформації про студентів.
-
     Створіть клас Student з полями ID, name та grades.
-
     Створіть клас GradeBook, який містить HashMap<String, Student> для зберігання студентів.
     Реалізуйте методи для додавання, видалення та пошуку студентів за ID.
     Реалізуйте метод для виведення всіх студентів та їх оцінок.*/
-    final HashMap<String, Student> GradeBook;
-    GradeBook(HashMap<String, Student> GradeBook){
-        this.GradeBook = GradeBook;
+    final HashMap<String, Student> gradeBook;
+    GradeBook(){
+        this.gradeBook = new HashMap<>();
     }
     public void addStudent(Student student){
-        this.GradeBook.put(student.id + "", student);
+        this.gradeBook.put(student.id + "", student);
     }
     public void deleteStudent(String ID){
-        this.GradeBook.remove(ID);
+        this.gradeBook.remove(ID);
     }
-    public Student findStudent(String ID){
-        return this.GradeBook.get(ID);
+    public Student findStudent(String ID){return this.gradeBook.get(ID);}
+    public void addGrade(String ID, int grade) {
+        Student student = this.gradeBook.get(ID);
+        student.grades.add(grade);
     }
     @Override
     public String toString(){
-        return this.GradeBook.toString();
+        return this.gradeBook.toString();
     }
     public void clearStudents(){
-        this.GradeBook.clear();
+        this.gradeBook.clear();
     }
 }
--- HashMaps/src/Main.java
+++ HashMaps/src/Main.java
@@ -5,19 +5,25 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        final GradeBook gradeBook = new GradeBook(new HashMap<String, Student>());
+        final GradeBook gradeBook = new GradeBook();
         final ArrayList<Integer> grades = new ArrayList<>();
         grades.add(1);
         grades.add(2);
         grades.add(3);
+        final ArrayList<Integer> grades1 = new ArrayList<>();
+        grades1.add(1);
+        grades1.add(2);
+        grades1.add(3);
         gradeBook.addStudent(new Student(1, "John", grades));
-        gradeBook.addStudent(new Student(2, "Jack", grades));
+        gradeBook.addStudent(new Student(2, "Jack", grades1));
         gradeBook.addStudent(new Student(3, "Eli", grades));
         gradeBook.addStudent(new Student(4, "George", grades));
         System.out.println(gradeBook);
         gradeBook.deleteStudent("1");
         System.out.println(gradeBook);
         System.out.println(gradeBook.findStudent("3"));
+        gradeBook.addGrade("2", 12);
+        System.out.println(gradeBook);
         gradeBook.clearStudents();
         System.out.println(gradeBook);
     }
