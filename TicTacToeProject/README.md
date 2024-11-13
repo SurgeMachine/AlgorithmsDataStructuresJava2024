531197ad745c9342ca116285044edfc2c2664d71
Surge <serhii.matchyn@stud.onu.edu.ua>
2024-11-13 07:48:15 +0200

    TicTacToe

---
+++ TicTacToeProject/README.md
@@ -0,0 +1,1 @@
+![Blank diagram.png](..%2F..%2F..%2FDownloads%2FBlank%20diagram.png)
---
+++ TicTacToeProject/src/Main.java
@@ -0,0 +1,14 @@
+package TicTacToeProject.src;
+
+import TicTacToeProject.src.tictactoe.players.Human;
+import TicTacToeProject.src.tictactoe.players.AI;
+import TicTacToeProject.src.tictactoe.players.Player;
+import TicTacToeProject.src.tictactoe.game.TicTacToe;
+public class Main {
+    public static void main(String[] args) {
+        Player player1 = new Human("Игрок 1", 'X');
+        Player player2 = new AI("Компьютер", 'O');
+        TicTacToe game = new TicTacToe(player1, player2);
+        game.playGame();
+    }
+}
