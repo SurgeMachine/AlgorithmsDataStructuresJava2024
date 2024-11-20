f21b0a3ed6ae23b4e7991447a6a4f1f184c03073
Surge <serhii.matchyn@stud.onu.edu.ua>
2024-11-20 08:50:28 +0200

    TicTacToe

--- TicTacToeProject/src/Main.java
+++ TicTacToeProject/src/Main.java
@@ -6,7 +6,7 @@ import TicTacToeProject.src.tictactoe.players.Player;
 import TicTacToeProject.src.tictactoe.game.TicTacToe;
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Human("Игрок 1", 'X');
+        Player player1 = new AI("Игрок 1", 'X');
         Player player2 = new AI("Компьютер", 'O');
         TicTacToe game = new TicTacToe(player1, player2);
         game.playGame();
--- TicTacToeProject/src/tictactoe/players/AI.java
+++ TicTacToeProject/src/tictactoe/players/AI.java
@@ -22,6 +22,11 @@ public class AI implements Player {
     }
 
     public int[] makeMove(char[][] board) {
+        int[] winningMove = findWinningMove(board, symbol);
+        if (winningMove != null) {
+            return winningMove;
+        }
+
         Random random = new Random();
         int row, col;
         do {
@@ -29,5 +34,65 @@ public class AI implements Player {
             col = random.nextInt(3);
         } while (board[row][col] != ' ');
         return new int[]{row, col};
+    }
+
+    private int[] findWinningMove(char[][] board, char symbol) {
+        for (int row = 0; row < 3; row++) {
+            int count = 0, emptyCol = -1;
+            for (int col = 0; col < 3; col++) {
+                if (board[row][col] == symbol) {
+                    count++;
+                } else if (board[row][col] == ' ') {
+                    emptyCol = col;
+                }
+            }
+            if (count == 2 && emptyCol != -1) {
+                return new int[]{row, emptyCol};
+            }
+        }
+
+        for (int col = 0; col < 3; col++) {
+            int count = 0, emptyRow = -1;
+            for (int row = 0; row < 3; row++) {
+                if (board[row][col] == symbol) {
+                    count++;
+                } else if (board[row][col] == ' ') {
+                    emptyRow = row;
+                }
+            }
+            if (count == 2 && emptyRow != -1) {
+                return new int[]{emptyRow, col};
+            }
+        }
+
+        int count = 0, emptyRow = -1, emptyCol = -1;
+        for (int i = 0; i < 3; i++) {
+            if (board[i][i] == symbol) {
+                count++;
+            } else if (board[i][i] == ' ') {
+                emptyRow = i;
+                emptyCol = i;
+            }
+        }
+        if (count == 2 && emptyRow != -1) {
+            return new int[]{emptyRow, emptyCol};
+        }
+
+        count = 0;
+        emptyRow = -1;
+        emptyCol = -1;
+        for (int i = 0; i < 3; i++) {
+            if (board[i][2 - i] == symbol) {
+                count++;
+            } else if (board[i][2 - i] == ' ') {
+                emptyRow = i;
+                emptyCol = 2 - i;
+            }
+        }
+        if (count == 2 && emptyRow != -1) {
+            return new int[]{emptyRow, emptyCol};
+        }
+
+        return null;
     }
 }
